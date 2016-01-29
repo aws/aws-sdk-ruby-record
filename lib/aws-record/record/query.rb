99a281ce1ac44cf9c9feae2dff34db27c321b412
Alex Wood <awood45@gmail.com>
2016-01-28 16:44:01 -0800

    Add Documentation to the Query Class

--- lib/aws-record/record/query.rb
+++ lib/aws-record/record/query.rb
@@ -21,11 +21,30 @@ module Aws
       end
 
       module QueryClassMethods
+
+        # This method calls
+        # {http://docs.aws.amazon.com/sdkforruby/api/Aws/DynamoDB/Client.html#query-instance_method Aws::DynamoDB::Client#query},
+        # populating the +:table_name+ parameter from the model class, and
+        # combining this with the other parameters you provide.
+        #
+        # @param [Hash] opts options to pass on to the client call to +#query+.
+        #   See the documentation above in the AWS SDK for Ruby V2.
+        # @return [Aws::Record::ItemCollection] an enumerable collection of the
+        #   query result.
         def query(opts)
           query_opts = opts.merge(table_name: table_name)
           ItemCollection.new(:query, query_opts, self, dynamodb_client)
         end
 
+        # This method calls
+        # {http://docs.aws.amazon.com/sdkforruby/api/Aws/DynamoDB/Client.html#scan-instance_method Aws::DynamoDB::Client#scan},
+        # populating the +:table_name+ parameter from the model class, and
+        # combining this with the other parameters you provide.
+        #
+        # @param [Hash] opts options to pass on to the client call to +#scan+.
+        #   See the documentation above in the AWS SDK for Ruby V2.
+        # @return [Aws::Record::ItemCollection] an enumerable collection of the
+        #   scan result.
         def scan(opts = {})
           scan_opts = opts.merge(table_name: table_name)
           ItemCollection.new(:scan, scan_opts, self, dynamodb_client)
