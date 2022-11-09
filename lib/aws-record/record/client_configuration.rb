ee85af7eb3139ae4110b3cb8e89b46e8067b7c3d
Juli Tera <terajul@amazon.com>
2022-11-09 12:51:43 -0500

    Add docs for dynamodb client inheritance

--- lib/aws-record/record/client_configuration.rb
+++ lib/aws-record/record/client_configuration.rb
@@ -26,6 +26,7 @@ module Aws
       # in doubt, call this method to ensure your client is configured the way
       # you want it to be configured.
       #
+      # *Note*: Can be inherited to child models if defined in parent model.
       # @param [Hash] opts the options you wish to use to create the client.
       #  Note that if you include the option +:client+, all other options
       #  will be ignored. See the documentation for other options in the
@@ -45,10 +46,12 @@ module Aws
       end
 
       # Gets the
-      # {https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/DynamoDB/Client.html}
+      # {https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/DynamoDB/Client.html Client}
       # instance that Transactions use. When called for the first time, if
       # {#configure_client} has not yet been called, will configure a new
       # client for you with default parameters.
+      #
+      # *Note*: Can be inherited to child models if defined in parent model by using {#configure_client}.
       #
       # @return [Aws::DynamoDB::Client] the Amazon DynamoDB client instance.
       def dynamodb_client
