5bf0b92e69fa43fa67f499f8468a04e40706d2c6
Juli Tera <terajul@amazon.com>
2022-11-15 16:09:21 -0500

    Update to add inheritance helper for dynamodb client

--- lib/aws-record/record/client_configuration.rb
+++ lib/aws-record/record/client_configuration.rb
@@ -35,7 +35,7 @@ module Aws
       # @option opts [Aws::DynamoDB::Client] :client allows you to pass in your
       #  own pre-configured client.
       def configure_client(opts = {})
-        if self.class != Module && self.superclass.include?(Aws::Record) && opts.empty? &&
+        if self.class != Module && Aws::Record.extends_record?(self) && opts.empty? &&
           self.superclass.instance_variable_get('@dynamodb_client')
             @dynamodb_client = self.superclass.instance_variable_get('@dynamodb_client')
         else
