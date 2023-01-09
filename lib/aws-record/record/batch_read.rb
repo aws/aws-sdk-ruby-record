6430ae1043e411ee74995392b91b7c02e232ccf6
Juli Tera <terajul@amazon.com>
2023-01-09 13:01:17 -0500

    Update syntax to improve readability

--- lib/aws-record/record/batch_read.rb
+++ lib/aws-record/record/batch_read.rb
@@ -117,8 +117,7 @@ module Aws
       def update_unprocessed_keys(keys)
         keys.each do |table_name, table_values|
           table_values.keys.each do |key|
-            formatted_key = { keys: key, table_name: table_name}
-            unprocessed_keys << formatted_key
+            unprocessed_keys << { keys: key, table_name: table_name}
           end
         end
       end
